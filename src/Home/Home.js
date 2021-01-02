ab3e6058cea349f6577a9540240905d98a702d76
Nasalis <davidossantosfreitas@hotmail.com>
2021-01-02 02:20:00 -0300

    Home atualizada

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 import './home.css'
 
-import { background } from '../images/background.png'
-
 export default function Home() {
     return (
         <div className="home">
@@ -20,6 +18,35 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="content">
+                <div className="info-cards">
+                    <div className="card">
+                        <div className="head">
+                            <i class="far fa-images"></i>
+                            <h2>Images</h2>
+                        </div>
+                        <p>Each card in the list has a image showing the selected pokemon. That way the identification is easier for those unknown pokemons!</p>
+                    </div>
+
+                    <div className="card">
+                        <div className="head">
+                            <i class="far fa-plus-square"></i>
+                            <h2>Stats</h2>
+                        </div>
+                        <p>Have you ever wondered what the status of a pokemon is? Now you can see some information about it to improve your pokemon master skills</p>
+                    </div>
+
+                    <div className="card">
+                        <div className="head">
+                            <i class="fas fa-shield-alt"></i>
+                            <h2>Abilities</h2>
+                        </div>
+                        <p>Can not remember how many abilities a specific pokemon has? Or have you forget any of then? The cards will help you e prepare yourself to battle!</p>
+                    </div>
+                </div>
+                <svg className="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#7a1818" fill-opacity="0.7" d="M0,256L48,229.3C96,203,192,149,288,144C384,139,480,181,576,165.3C672,149,768,75,864,85.3C960,96,1056,192,1152,224C1248,256,1344,224,1392,208L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
             </div>
         </div>
     )
