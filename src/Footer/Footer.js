fd2d7fbcdecf7a6c2b16715c16334d595e811af4
Nasalis <davidossantosfreitas@hotmail.com>
2021-01-02 02:55:07 -0300

    Footer inicial adicionado

---
+++ src/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './footer.css'
+
+export default function footer() {
+    return (
+        <footer>
+            <div className="contact">
+                <span>What's the real Ash's age?</span>
+                <div className="social-media">
+                    <ul>
+                        <li><i class="fab fa-facebook-square"></i></li>
+                        <li><i class="fab fa-instagram"></i></li>
+                        <li><i class="fab fa-twitter"></i></li>
+                        <li><i class="fab fa-github"></i></li>
+                        <li><i class="fab fa-youtube"></i></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
